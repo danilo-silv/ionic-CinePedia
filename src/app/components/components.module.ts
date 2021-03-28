a9cbc6104cf88d39454a1a14b38477a14ee65a3c
Danilo Silva <danilo.silva@laborit.com.br>
2021-03-28 17:02:08 -0300

    crating modules components

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { LogoComponent } from './logo/logo.component';
+
+@NgModule({
+  imports: [IonicModule],
+  exports: [LogoComponent],
+  declarations: [LogoComponent],
+})
+export class ComponentsModule {}
---
+++ src/app/components/logo/logo.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.scss'],
+})
+export class LogoComponent implements OnInit {
+  @Input()
+  textLogo: string;
+  constructor() {}
+
+  ngOnInit() {}
+}
