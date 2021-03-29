fe0aed12a622b321c34c1ec6d7220e110b1cd104
Danilo Silva <danilo.silva@laborit.com.br>
2021-03-28 23:08:00 -0300

    inserting fixed header structure for pages

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { LogoComponent } from './illustration/logo/logo.component';
 import { HomeCinemaComponent } from './illustration/home-cinema/home-cinema.component';
+import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [IonicModule],
-  exports: [LogoComponent, HomeCinemaComponent],
-  declarations: [LogoComponent, HomeCinemaComponent],
+  imports: [IonicModule, CommonModule],
+  exports: [LogoComponent, HomeCinemaComponent, HeaderComponent],
+  declarations: [LogoComponent, HomeCinemaComponent, HeaderComponent],
 })
 export class ComponentsModule {}
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit, DoCheck {
+  public showHeader: boolean = true;
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  ngDoCheck() {
+    const { url } = this.router;
+
+    if (url === '/' || url === 'welcome') {
+      this.showHeader = false;
+    } else {
+      this.showHeader = true;
+    }
+  }
+}
