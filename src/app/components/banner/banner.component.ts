28e17f266610fba938c52050933f6987c28adf83
Danilo Silva <danilo.silva@laborit.com.br>
2021-03-29 23:51:01 -0300

    creating <Banner /> component

---
+++ src/app/components/banner/banner.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { SliderOptions } from 'src/app/models/slider-options';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss'],
+})
+export class BannerComponent implements OnInit {
+  @Input() public bannerList: Array<any>; //changing type data berfore
+
+  public type: string = 'banner';
+  public slidesOptions: SliderOptions = {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    speed: 400,
+    pager: true,
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
+}
