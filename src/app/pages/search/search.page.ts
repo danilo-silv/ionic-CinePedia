c47b099358e4406082de1707cecef50a2bfadd1a
Danilo Silva <danilo.silva@laborit.com.br>
2021-04-04 00:47:22 -0300

    update list for serach user

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Search } from 'src/app/models/search';
+import { CatalogService } from 'src/app/service/catalog.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
+  public searchList: Search[] = [];
+  public currentSearch = '';
+  public filteredSearch = this.searchList;
 
-  constructor() { }
+  constructor(private catalogService: CatalogService) {}
 
   ngOnInit() {
+    this.searchList = this.catalogService.getCardsMovie();
+    console.log(this.searchList);
   }
 
+  public searchItem() {
+    if (this.currentSearch === '') {
+      this.filteredSearch = [];
+    } else {
+      const lowercased = this.currentSearch.toLowerCase();
+      this.filteredSearch = this.searchList.filter((item) =>
+        item.title.toLowerCase().includes(lowercased)
+      );
+    }
+  }
 }
