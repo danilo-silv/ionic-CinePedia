63b9afbc57688e9d8d7ecc207cece6aab04a44ea
Danilo Silva <danilo.silva@laborit.com.br>
2021-04-11 17:34:05 -0300

    adding function for directing the search result to the detail screen

--- src/app/components/card-search/card-search.component.ts
+++ src/app/components/card-search/card-search.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DetailItem } from 'src/app/models/search';
 
 @Component({
   selector: 'app-card-search',
@@ -6,12 +8,15 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./card-search.component.scss'],
 })
 export class CardSearchComponent implements OnInit {
-  @Input() public title: string;
-  @Input() public image: string;
-  @Input() public description: string;
-  @Input() public producer: string;
+  @Input() public search: DetailItem;
 
-  constructor() {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {}
+
+  handleClick(id: number) {
+    this.router.navigate([`../details/genre/${id}`], {
+      relativeTo: this.route,
+    });
+  }
 }
