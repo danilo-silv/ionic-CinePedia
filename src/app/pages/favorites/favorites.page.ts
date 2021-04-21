c1f8d42da083581c26ead185cec8596a11d8e5c3
GuilhermeEPS <guilhermeeps@hotmail.com>
2021-04-21 17:24:15 -0300

    Inserting logic to get movies and series

--- src/app/pages/favorites/favorites.page.ts
+++ src/app/pages/favorites/favorites.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { CatalogService } from 'src/app/service/catalog.service';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.page.html',
   styleUrls: ['./favorites.page.scss'],
 })
-export class FavoritesPage implements OnInit {
 
-  constructor() { }
+//Precisamos dos filmes e series - GET NA API - OK
+//Precisamos listar os filmes e series - DISPONIBILIZAR PARA O USUARIO
+//Precisamos criar o filtro de series e filmes
+//Precisamos filtrar os favoritos do usuario
+
+export class FavoritesPage implements OnInit {
+  public favorites: Array<any>;
+
+  constructor(private catalogService: CatalogService) { }
 
   ngOnInit() {
+    this.catalogService.listGenre().subscribe((genre: any[])=>{
+      this.favorites = genre
+    });
   }
 
 }
