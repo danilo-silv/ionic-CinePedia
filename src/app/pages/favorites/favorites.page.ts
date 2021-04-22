5e2211bc7cb4c783bfe6ec102d70d1a03ff607b5
Danilo Silva <danilo.silva@laborit.com.br>
2021-04-21 22:08:12 -0300

    changing the image height of the favorites content

--- src/app/pages/favorites/favorites.page.ts
+++ src/app/pages/favorites/favorites.page.ts
@@ -8,21 +8,19 @@ import { CatalogService } from 'src/app/service/catalog.service';
 })
 
 //Precisamos dos filmes e series - GET NA API - OK
-//Precisamos listar os filmes e series - DISPONIBILIZAR PARA O USUARIO
+//Precisamos listar os filmes e series - DISPONIBILIZAR PARA O USUARIO - OK
 //Precisamos criar o filtro de series e filmes
 //Precisamos filtrar os favoritos do usuario
-
 export class FavoritesPage implements OnInit {
   public favorites: Array<any>;
   public fetch: boolean = true;
 
-  constructor(private catalogService: CatalogService) { }
+  constructor(private catalogService: CatalogService) {}
 
   ngOnInit() {
-    this.catalogService.listGenre().subscribe((genre: any[])=>{
-      this.favorites = genre
-      // this.fetch = false;
+    this.catalogService.listGenre().subscribe((genre: any[]) => {
+      this.favorites = genre;
+      this.fetch = false;
     });
   }
-
 }
